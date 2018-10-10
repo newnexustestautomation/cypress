1eb5129e6f0a7cb6ab25c1cb5601f11f78ebff5f
marvinleito <m.leito@gmail.com>
2018-10-10 13:41:00 +0200

    eerste commit!

--- cypress/integration/cypress_demo_iphone6.spec.js
+++ cypress/integration/cypress_demo_iphone6.spec.js
@@ -1,7 +1,8 @@
 
-context('Demo RDW', () => {
+context('Demo RDW - iPhone 6', () => {
   beforeEach(() => {
     cy.visit('https://snellemotorboten.rdw.nl')
+    cy.viewport('iphone-6')  // Set viewport to 375px x 667px
   })
 
    // The most commonly used query is 'cy.get()', you can
@@ -23,16 +24,13 @@ context('Demo RDW', () => {
         cy.get('button:first').should('contain', 'Particulier').click()
         cy.get('button:last').should('contain', 'Rechtspersoon').click()
       })
-      // particulier
-      //cy.get('.btn-group > :nth-child(1)').should('contain', 'Particulier').click()
-      //rechtspersoon
-      //cy.get('.btn-group > :nth-child(2)').should('contain', 'Rechtspersoon').click({multiple: true})
-
-      //cy.get('.ng-untouched > .btn-group > :nth-child(1)')
     })
 
-//click on submit button
-    it('cy.get() - Submit button', () => {
-        cy.get('button[type=submit').should('contain', 'Doorgaan').click()
-    })
+
+    //verstuur
+    //it('cy.get() - Submit button', () => {
+    //    cy.get('button[type=submit').should('contain', 'Doorgaan').click()
+    //})
+
+
 })
